9b2a36102117e235c8c9a220ef7155d73aedb223
imyoi <zr.colaaa@gmail.com>
2023-06-08 19:34:56 +0900

    feat: JPA, 스프링 데이터 JPA 적용

--- src/main/java/spring/springbasic/SpringConfig.java
+++ src/main/java/spring/springbasic/SpringConfig.java
@@ -1,31 +1,32 @@
 package spring.springbasic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import spring.springbasic.repository.JdbcTemplateMemberRepository;
 import spring.springbasic.repository.MemberRepository;
 import spring.springbasic.service.MemberService;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
 
-    private final DataSource dataSource;
-
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource =  dataSource;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository =  memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
+/*
     @Bean
     public MemberRepository memberRepository() {
 //      return new MemoryMemberRepository();
 //      return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//      return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
+ */
 }
--- src/main/java/spring/springbasic/service/MemberService.java
+++ src/main/java/spring/springbasic/service/MemberService.java
@@ -1,13 +1,13 @@
 package spring.springbasic.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import spring.springbasic.domain.Member;
 import spring.springbasic.repository.MemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
