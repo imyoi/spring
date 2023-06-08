dc6a96d606cca13494674ae3decbf0e60f564d74
imyoi <zr.colaaa@gmail.com>
2023-06-08 15:27:16 +0900

    feat: 순수 JDBC, JdbcTemplate

--- src/main/java/spring/springbasic/SpringConfig.java
+++ src/main/java/spring/springbasic/SpringConfig.java
@@ -2,12 +2,20 @@ package spring.springbasic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring.springbasic.repository.JdbcTemplateMemberRepository;
 import spring.springbasic.repository.MemberRepository;
-import spring.springbasic.repository.MemoryMemberRepository;
 import spring.springbasic.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource =  dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +24,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//      return new MemoryMemberRepository();
+//      return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
